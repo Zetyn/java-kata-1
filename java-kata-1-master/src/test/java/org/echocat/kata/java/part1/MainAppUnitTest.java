6d97daf647f1350940736b076f78fbca0f8f6948
Zetyn <rost.zgyrsk@gmail.com>
2021-04-27 16:02:28 +0300

    part1

---
+++ java-kata-1-master/src/test/java/org/echocat/kata/java/part1/MainAppUnitTest.java
@@ -0,0 +1,15 @@
+package org.echocat.kata.java.part1;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MainAppUnitTest {
+
+    @Test
+    public void testGetHelloWorldText() {
+
+    }
+
+}
