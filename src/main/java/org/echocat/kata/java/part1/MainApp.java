2a969976b3ccf80be93ba38ad2215f2d3c0f7e20
Zetyn <48289855+Zetyn@users.noreply.github.com>
2021-04-29 15:04:36 +0300

    Add files via upload

---
+++ src/main/java/org/echocat/kata/java/part1/MainApp.java
@@ -0,0 +1,20 @@
+package org.echocat.kata.java.part1;
+
+import java.io.IOException;
+
+public class MainApp {
+
+    public static void main(String[] args) throws IOException  {
+        ReadCSV_SortList b = new ReadCSV_SortList();
+        System.out.println("Books");
+        b.readBooks();
+        System.out.println("Magazines");
+        b.readMagazines();
+        System.out.println("Authors");
+        b.readAuthors();
+        b.sort();
+        System.out.println("search");
+        b.search();
+    }
+
+}
