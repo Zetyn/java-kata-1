5d436561ba3a559850860dcb113a7bf98f13e0fb
Zetyn <48289855+Zetyn@users.noreply.github.com>
2021-05-25 16:21:25 +0300

    rename

--- src/main/java/org/echocat/kata/java/part1/ReadOutput.java
+++ src/main/java/org/echocat/kata/java/part1/ReadOutput.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class ReadCSVSortList  {
+public class ReadOutput  {
 
     List<Books> bookList = new ArrayList<>();
     List<Books> bookSort = new ArrayList<>(bookList);
