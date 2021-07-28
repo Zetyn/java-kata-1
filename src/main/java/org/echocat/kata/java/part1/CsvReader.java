26b634b1cc02e6bd3ec7e8ff96eb03e4b60bc0ff
Zetyn <rost.zgyrsk@gmail.com>
2021-07-28 12:57:03 +0300

    update CsvReader and MainApp

--- src/main/java/org/echocat/kata/java/part1/CsvReader.java
+++ src/main/java/org/echocat/kata/java/part1/CsvReader.java
@@ -1,7 +1,5 @@
 package org.echocat.kata.java.part1;
 
-import java.util.Scanner;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -23,35 +21,21 @@ public class CsvReader {
     BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/org/echocat/kata/java/part1/data/books.csv"));
         
     String line;
-    Scanner scanner;
-    int index = 0;
     int k = 0;
-    
 
     while ((line = reader.readLine())!= null) {
         Book book = new Book();
-        scanner = new Scanner(line);
-        scanner.useDelimiter(";");
+        String[] strings = line.split(";");
         if (k == 1) {
-            while (scanner.hasNext()) {
-                String data = scanner.next();
-                if(index == 0) {
-                    book.setTitle(data);
-                } else if (index == 1) {
-                    book.setIsbn(data);
-                } else if (index == 2) {
-                    String[] authorsArray = data.split(",");
-                    emails = new ArrayList<>();
-                    for (String email : authorsArray) {
-                        emails.add(email);
-                    }
-                    book.setAuthor(emails);
-                } else if(index == 3) {
-                    book.setDescription(data);
-                } else {System.out.println("incorrect data " + data);}
-                index++;
+            book.setTitle(strings[0]);
+            book.setIsbn(strings[1]);
+            String[] authorsArray = strings[2].split(",");
+            emails = new ArrayList<>();
+            for (String email : authorsArray) {
+                emails.add(email);
             }
-            index = 0;
+            book.setAuthor(emails);
+            book.setDescription(strings[3]);
             books.add(book);
         } else {k = 1;}
     }
@@ -63,37 +47,24 @@ public class CsvReader {
         BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/org/echocat/kata/java/part1/data/magazines.csv"));
             
         String line;
-        Scanner scanner;
-        int index = 0;
         int k = 0;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
 
         while ((line = reader.readLine())!= null) {
             Magazine magazine = new Magazine();
-            scanner = new Scanner(line);
-            scanner.useDelimiter(";");
+            String[] strings = line.split(";");
             if (k == 1) {
-                while (scanner.hasNext()) {
-                    String data = scanner.next();
-                        if(index == 0) {
-                            magazine.setTitle(data);
-                        } else if (index == 1) {
-                            magazine.setIsbn(data);
-                        } else if (index == 2) {
-                            String[] str = data.split(",");
-                            emails = new ArrayList<>();
-                            for (String email : str) {
-                                emails.add(email);
-                            }
-                            magazine.setAuthor(emails);
-                        } else if(index == 3) {
-                            LocalDate date = LocalDate.parse(data, formatter);
-                            magazine.setPublishedAt(date);
-                        }  else {System.out.println("Некоректні дані " + data);}
-                        index++;
-                    }
-                    index = 0;
-                    magazines.add(magazine);
+                magazine.setTitle(strings[0]);
+                magazine.setIsbn(strings[1]);
+                String[] str = strings[2].split(",");
+                emails = new ArrayList<>();
+                for (String email : str) {
+                    emails.add(email);
+                }
+                magazine.setAuthor(emails);
+                LocalDate date = LocalDate.parse(strings[3], formatter);
+                magazine.setPublishedAt(date);
+                magazines.add(magazine);
             } else {k = 1;}
 
         }
@@ -105,27 +76,15 @@ public class CsvReader {
             BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/org/echocat/kata/java/part1/data/authors.csv"));
 
             String line;
-            Scanner scanner;
-            int index = 0;
             int k = 0;
 
             while((line = reader.readLine())!=null) {
                 Author author = new Author();
-                scanner = new Scanner(line);
-                scanner.useDelimiter(";");
+                String[] strings = line.split(";");
                 if (k == 1) {
-                    while(scanner.hasNext()) {
-                        String data = scanner.next();
-                        if (index == 0) {
-                            author.setEmail(data);
-                        } else if (index == 1) {
-                            author.setFirstName(data);
-                        } else if (index == 2) {
-                            author.setLastName(data);
-                        } else {System.out.println("Некоректні дані " + data);}
-                        index++;
-                    }
-                    index = 0;
+                    author.setEmail(strings[0]);
+                    author.setFirstName(strings[1]);
+                    author.setLastName(strings[2]);
                     authors.add(author);
                 } else {k = 1;}
             }
--- src/main/java/org/echocat/kata/java/part1/MainApp.java
+++ src/main/java/org/echocat/kata/java/part1/MainApp.java
@@ -1,19 +1,33 @@
 package org.echocat.kata.java.part1;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class MainApp {
 
-    public static void main(String[] args) throws IOException  {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class,args);
+    }
+
+    @RequestMapping("/")
+    public void consolePrint() throws IOException {
         CsvReader reader = new CsvReader();
 
         List<Author> authors = reader.readAuthors();
-        List<Book> books = reader.readBooks();
-        List<Magazine> magazines = reader.readMagazines();
-        List<TitleIsbnAuthorModel> booksAndMagazinesSort = new ArrayList<>();
+        List <Book> books = reader.readBooks();
+        List <Magazine> magazines = reader.readMagazines();
+        List <TitleIsbnAuthorModel> booksAndMagazinesSort = new ArrayList<>();
         booksAndMagazinesSort.addAll(books);
         booksAndMagazinesSort.addAll(magazines);
         Sort.sortBooksAndMagazinesByTitle(booksAndMagazinesSort);
@@ -37,6 +51,6 @@ public class MainApp {
         printer.print("\nFound by author\n" + bookByAuthorResults);
 
         in.close();
-
     }
+
 }
